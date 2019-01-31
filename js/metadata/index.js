2b05e54b02232186671c9d0ca24823da641238c9
Tyler Toth <tyler@cloud-elements.com>
2019-01-30 19:36:34 -0600

    :squirrel: Added swagger UI

--- js/metadata/index.js
+++ js/metadata/index.js
@@ -8,6 +8,17 @@ const sendTableMsg = () => {
   }
 }
 
+const sendSwaggerUi = () => {
+  var views = chrome.extension.getViews({ type: "popup" });
+  if (views && views.length > 0) {
+    chrome.runtime.sendMessage({
+      type: 'popout',
+      url: '/html/metadata/swaggerui.html'
+    });
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('metadata').addEventListener('click', sendTableMsg);
+  document.getElementById('swaggerUi').addEventListener('click', sendSwaggerUi);
 }, false);
---
+++ js/metadata/swaggerui.js
@@ -0,0 +1,39 @@
+import {getCached} from '../utilities/ce-utils.js';
+
+function getSwagger() {
+  var elementId = document.getElementById('elementId').value;
+
+  chrome.storage.sync.set({
+    "elementId": elementId
+  });
+
+  getCached('ce-eb-ub-env', env => {
+    const ui = SwaggerUIBundle({
+      url: `${env}/elements/api-v2/elements/${elementId}/docs?version=-1`,
+      dom_id: '#swagger-ui',
+      docExpansion: 'none',
+      tagsSorter: 'alpha',
+      presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIStandalonePreset
+      ]
+    });
+
+    window.ui = ui
+  });
+}
+
+window.onload = function () {
+  chrome.storage.sync.get("elementId", function (items) {
+    if (!chrome.runtime.error) {
+      console.log(items);
+      if (items.elementId) {
+        document.getElementById("elementId").value = items.elementId;
+      }
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById("getSwagger").addEventListener("click", getSwagger);
+}, false);
